5391d44463ff0f68b5b1baa27fcd111a1de67bd1
Jonas Nick <jonasd.nick@gmail.com>
2022-08-12 13:35:17 +0000

    reference code: Reenable nonce_gen and nonce_agg test vectors

--- bip-musig2/reference.py
+++ bip-musig2/reference.py
@@ -741,8 +741,8 @@ def test_sign_and_verify_random(iters: int) -> None:
 if __name__ == '__main__':
     # TODO
     test_key_agg_vectors()
-    # test_nonce_gen_vectors()
-    # test_nonce_agg_vectors()
+    test_nonce_gen_vectors()
+    test_nonce_agg_vectors()
     # test_sign_verify_vectors()
     # test_tweak_vectors()
     # test_sig_agg_vectors()
